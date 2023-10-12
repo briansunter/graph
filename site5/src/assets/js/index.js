cefe6cdaa2cfd66024682f26da28101388f3c385
Brian Sunter <2816289+briansunter@users.noreply.github.com>
2023-10-12 10:47:37 -1000

    works with no slash

---
+++ site5/src/assets/js/index.js
@@ -0,0 +1,10 @@
+
+import Alpine from 'alpinejs'
+ 
+window.Alpine = Alpine
+
+// Add Alpine extensions here
+ 
+Alpine.start()
+
+console.log('hello from index.js')
---
+++ site5/src/posts/my-first-post.md
@@ -0,0 +1,9 @@
+---
+layout: base
+title: My First Post
+tags: post
+---
+
+Hello from {{ title }}!
+
+{% island 'Counter.tsx', 'client:load' %} {% endisland %}
