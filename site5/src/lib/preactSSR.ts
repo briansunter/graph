c343214fc51810d1840ae4a60fa14d471bce3490
Brian Sunter <2816289+briansunter@users.noreply.github.com>
2023-10-17 16:27:28 -1000

    Works with require

--- site5/src/lib/preactSSR.ts
+++ site5/src/lib/preactSSR.ts
@@ -1,48 +1,29 @@
 import { EleventyConfig } from "../../eleventy";
 import uuid from "short-uuid";
-
 import { h } from "preact";
 import render from "preact-render-to-string";
-import ts from "typescript";
-import fs from "fs";
-import vm from "vm";
-export default function (eleventyConfig: EleventyConfig, options = {}) {
-  eleventyConfig.addPassthroughCopy("src/scripts");
+import path from "path";
+
+export default function (
+  eleventyConfig: EleventyConfig,
+  options = { componentDir: "src/components" }
+) {
+  eleventyConfig.addPassthroughCopy(options.componentDir);
   eleventyConfig.addAsyncShortcode("react", async function (component) {
-    let filename = `src/scripts/${component.comp}`;
+    let filename = path.resolve(process.cwd(), options.componentDir, component.comp);
+ 
+    // Dynamically import the component
+    const Component = require(filename).default as preact.ComponentType<any>;
 
-    const source = fs.readFileSync(filename, "utf8");
-
-    const serverResult = ts.transpileModule(source, {
-      compilerOptions: {
-        module: ts.ModuleKind.Node16,
-        jsx: ts.JsxEmit.React,
-        esModuleInterop: true,
-      },
-    });
-
-    const context = {
-      require: require,
-      module: {},
-      console: console,
-      exports: {} as any,
-    };
-
-    // Run the code in the new context
-    vm.runInNewContext(serverResult.outputText, context);
-
-    // Access the exported Component function
-    const Component = context.exports.default as preact.ComponentType<any>;
-    const componentInstance = h(Component, component.props||{});
+    const componentInstance = h(Component, component.props || {});
     const componentHTML = render(componentInstance);
-    console.log("foobar",componentHTML)
 
     const domId = `pr-${uuid().new()}`;
 
     return `
   <script type="module">
-  import c from '/scripts/${component.comp}'
-  import hydrate from '/scripts/hydrate.ts'
+  import c from '/components/${component.comp}'
+  import hydrate from '/components/hydrate.ts'
   const domNode = document.getElementById("${domId}");
 
   if (!domNode) {
