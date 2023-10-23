7c75f13cca07e73a3961d098ab5efc5bea4bf332
Brian Sunter <2816289+briansunter@users.noreply.github.com>
2023-10-22 21:00:34 -1000

    Fix virtual table

--- site5/src/components/AllPages.tsx
+++ site5/src/components/AllPages.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { DateTime } from 'luxon';
 import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
 import { IconDefinition, faSort, faSortDown, faSortUp } from '@fortawesome/free-solid-svg-icons'
 import { useState, useEffect, useMemo, useRef, useTransition } from 'react';
@@ -6,9 +7,9 @@ import Fuse from 'fuse.js';
 import fuzzysort from 'fuzzysort'
 import uFuzzy from '@leeoniya/ufuzzy'
 import {
-  useReactTable, ColumnDef, flexRender, RowModel, Table, getCoreRowModel, SortingState, getSortedRowModel
+  useReactTable, ColumnDef, flexRender, RowModel, Table, getCoreRowModel, SortingState, getSortedRowModel, Row
 } from '@tanstack/react-table';
-import { useVirtual } from 'react-virtual';
+import { useVirtualizer } from '@tanstack/react-virtual';
 
 import { Post } from '../types';
 
@@ -46,7 +47,7 @@ const Search: React.FC<Props> = ({ allPosts }): JSX.Element => {
   const [isPending, startTransition] = useTransition();
   const [search, setSearch] = useState('');
   const [results, setResults] = useState<ResultPost[]>(initialPosts);
-  const [sorting, setSorting] = React.useState<SortingState>([{ id: 'date', desc: true}])
+  const [sorting, setSorting] = React.useState<SortingState>([{ id: 'date', desc: true }])
   const tableContainerRef = useRef<HTMLDivElement>(null);
 
 
@@ -55,7 +56,7 @@ const Search: React.FC<Props> = ({ allPosts }): JSX.Element => {
       header: 'Cover Image',
       accessorKey: 'coverimage',
       cell: info =>
-        <img className="cellImage" src={info.getValue() as string} alt={info.row.original.title} loading='lazy'/>
+        <img className="cellImage" src={info.getValue() as string} alt={info.row.original.title} loading='lazy' />
     },
     {
       header: (headerInfo) => (
@@ -63,10 +64,8 @@ const Search: React.FC<Props> = ({ allPosts }): JSX.Element => {
           className='flex flex-row items-center'
           onClick={headerInfo.column.getToggleSortingHandler()}>
           Title
-          {{
-            asc: <SortIcon icon={faSortUp} />,
-            desc: <SortIcon icon={faSortDown} />,
-          }[headerInfo.column.getIsSorted() as string] ?? <SortIcon icon={faSort} />}
+          {
+          [headerInfo.column.getIsSorted() as string] ?? <SortIcon icon={faSort} />}
         </div>
       ),
       accessorKey: 'title',
@@ -76,59 +75,55 @@ const Search: React.FC<Props> = ({ allPosts }): JSX.Element => {
       size: 1,
       header: 'Description',
       accessorKey: 'description',
-      cell: info =><p className="mb-2 text-sm grow">{info.getValue() as string}</p>
+      cell: info => <p className="mb-2 text-sm">{info.getValue() as string}</p>
     },
     {
       size: 1,
       header: (headerInfo) => (
         <div
-          className='flex flex-row items-center grow'
+          className='flex flex-row items-center'
           onClick={headerInfo.column.getToggleSortingHandler()} // Add this line
         >
           Published
-          {{
-            asc: <SortIcon icon={faSortUp} />,
-            desc: <SortIcon icon={faSortDown} />,
-          }[headerInfo.column.getIsSorted() as string] ?? <SortIcon icon={faSort} />}
+          {
+          [headerInfo.column.getIsSorted() as string] ?? <SortIcon icon={faSort} />}
         </div>
       ),
       accessorKey: 'date',
-      cell: info => <p className="text-sm text-gray-500 grow">Published: {info.getValue() as string}</p>,
+      cell: info => {
+        const date = DateTime.fromJSDate(info.getValue() as Date);
+        const formattedDate = date.toFormat('yyyy LLL dd');
+        return <p className="text-sm text-gray-500">Published: {formattedDate}</p>;
+      },
     },
     {
       header: (headerInfo) => (
         <div
-          className='flex flex-row items-center grow'
+          className='flex flex-row items-center'
           onClick={headerInfo.column.getToggleSortingHandler()} // Add this line
         >
           Updated Date
-          {{
-            asc: <SortIcon icon={faSortUp} />,
-            desc: <SortIcon icon={faSortDown} />,
-          }[headerInfo.column.getIsSorted() as string] ?? <SortIcon icon={faSort} />}
+          {
+          [headerInfo.column.getIsSorted() as string] ?? <SortIcon icon={faSort} />}
         </div>
       ),
       accessorKey: 'lastModified',
-      cell: info => <p className="text-sm text-gray-500 grow">Updated: {info.getValue() as string}</p>,
+      cell: info => <p className="text-sm text-gray-500">Updated: {info.getValue() as string}</p>,
     },
     {
       header: (headerInfo) => (
         <div
-          className='flex flex-row items-center grow'
+          className='flex flex-row items-center'
           onClick={headerInfo.column.getToggleSortingHandler()} // Add this line
         >
           Word Count
-          {{
-            asc: <SortIcon icon={faSortUp} />,
-            desc: <SortIcon icon={faSortDown} />,
-          }[headerInfo.column.getIsSorted() as string] ?? <SortIcon icon={faSort} />}
         </div>
       ),
       accessorKey: 'wordCount',
       cell: info => {
         const post = info.row.original;
         const wordCount = post.wordCount;
-        return <p className='grow'>{wordCount}</p>;
+        return <p className=''>{wordCount}</p>;
       },
     },
     {
@@ -138,7 +133,7 @@ const Search: React.FC<Props> = ({ allPosts }): JSX.Element => {
         const cellInfo = info.getValue() as string[];
 
         return (
-          <div className="flex flex-wrap grow">
+          <div className="flex flex-wrap">
             {cellInfo?.map((tag, index) => (
               <span key={index} className="mr-2 text-sm text-gray-700">
                 #{tag}
@@ -163,16 +158,19 @@ const Search: React.FC<Props> = ({ allPosts }): JSX.Element => {
     onSortingChange: setSorting,
   });
   const { rows } = table.getRowModel();
-  const rowVirtualizer = useVirtual({
-    parentRef: tableContainerRef,
-    size: rows.length,
-    overscan: 40,
+  const rowVirtualizer = useVirtualizer({
+    getScrollElement: () => tableContainerRef.current,
+    count: rows.length,
+    estimateSize: React.useCallback(() => 35, []),
+    overscan: 50 
   });
 
-  const { virtualItems: virtualRows, totalSize } = rowVirtualizer;
+  // const { virtualItems: virtualRows, totalSize } = rowVirtualizer;
 
-  const paddingTop = virtualRows.length > 0 ? virtualRows?.[0]?.start || 0 : 0;
-  const paddingBottom = virtualRows.length > 0 ? totalSize - (virtualRows?.[virtualRows.length - 1]?.end || 0) : 0;
+  const virtualItems = rowVirtualizer.getVirtualItems();
+
+  // const paddingTop = virtualRows.length > 0 ? virtualRows?.[0]?.start || 0 : 0;
+  // const paddingBottom = virtualRows.length > 0 ? totalSize - (virtualRows?.[virtualRows.length - 1]?.end || 0) : 0;
 
   const workerRef = useRef<Worker | null>(null);
 
@@ -209,8 +207,9 @@ const Search: React.FC<Props> = ({ allPosts }): JSX.Element => {
     debouncedPostMessage(worker, search);
 
   }, [search]);
+
   return (
-    <div className="p-4">
+    <div>
       <input
         className="w-full p-2 mb-4 border border-gray-300 rounded"
         type="text"
@@ -218,47 +217,76 @@ const Search: React.FC<Props> = ({ allPosts }): JSX.Element => {
         value={search}
         onChange={(e) => setSearch((e.target as HTMLInputElement).value)}
       />
-      <div ref={tableContainerRef} className='searchView'>
+    <div ref={tableContainerRef} className="container">
+      <div style={{ height: `${rowVirtualizer.getTotalSize()}px` }}>
         <table>
-          <thead className="bg-gray-200">
-            {table.getHeaderGroups().map(headerGroup => (
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
               <tr key={headerGroup.id}>
-                {headerGroup.headers.map(header => (
-                  <th className="px-2 py-2" key={header.id}>
-                    {flexRender(header.column.columnDef.header, header.getContext()) as JSX.Element}
-                  </th>
-                ))}
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th
+                      key={header.id}
+                      colSpan={header.colSpan}
+                      style={{ width: header.getSize() }}
+                    >
+                      {header.isPlaceholder ? null : (
+                        <div
+                          {...{
+                            className: header.column.getCanSort()
+                              ? 'flex flex-row items-center cursor-pointer select-none'
+                              : '',
+                            onClick: header.column.getToggleSortingHandler(),
+                          }}
+                        >
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                          {{asc: <SortIcon icon={faSortUp} />,
+                          false:           <SortIcon icon={faSort} />,
+                            desc: <SortIcon icon={faSortDown} />,
+                          }[header.column.getIsSorted() as string] ?? null}
+                        </div>
+                      )}
+                    </th>
+                  )
+                })}
               </tr>
             ))}
           </thead>
           <tbody>
-            {paddingTop > 0 && (
-              <tr>
-                <td style={{ height: `${paddingTop}px` }} />
-              </tr>
-            )}
-            {virtualRows.map(virtualRow => {
-              const row = rows[virtualRow.index];
+            {rowVirtualizer.getVirtualItems().map((virtualRow, index) => {
+              const row = rows[virtualRow.index] as Row<ResultPost>
               return (
-                <tr className="hover:bg-gray-100" key={row.id}>
-                  {row.getVisibleCells().map(cell => (
-                    <td className="border px-2 py-2" key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext()) as JSX.Element}
-                    </td>
-                  ))}
+                <tr
+                  key={row.id}
+                  style={{
+                    height: `${virtualRow.size}px`,
+                    transform: `translateY(${
+                      virtualRow.start - index * virtualRow.size
+                    }px)`,
+                  }}
+                >
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </td>
+                    )
+                  })}
                 </tr>
-              );
+              )
             })}
-            {paddingBottom > 0 && (
-              <tr>
-                <td style={{ height: `${paddingBottom}px` }} />
-              </tr>
-            )}
           </tbody>
         </table>
       </div>
     </div>
-  );
+    </div>
+  )
 };
 
 export default Search;
