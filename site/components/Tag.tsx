918dfce46e7cf621a2867d4d943af38970a6e1b2
Brian Sunter <2816289+briansunter@users.noreply.github.com>
2023-12-11 21:24:36 -1000

    Fix tag and h4 spacing

--- site/components/Tag.tsx
+++ site/components/Tag.tsx
@@ -1,7 +1,7 @@
 export const Tag = ({ text, shouldMargin = false }: { text: string, shouldMargin?: boolean }) => {
   const href = text === 'newsletter' ? '/newsletter' : `/tags/${text}`;
   return (
-    <a href={href} className={`inline-block text-xl lg:text-sm font-semibold text-blue-700 bg-blue-200 rounded-full px-8 py-4 lg:px-4 lg:py-2 cursor-pointer hover:bg-blue-300 hover:shadow-md transition duration-300 ease-in-out no-underline mr-2 ${shouldMargin ? 'mb-2' : ''}`}>
+    <a href={href} className={`inline-block text-xl lg:text-sm font-semibold text-blue-700 bg-blue-200 rounded-full px-8 py-3 lg:px-4 lg:py-2 cursor-pointer mb-2 lg:mb-0 hover:bg-blue-300 hover:shadow-md transition duration-300 ease-in-out no-underline mr-2 ${shouldMargin ? 'mb-2' : ''}`}>
       {text}
     </a>
   );
