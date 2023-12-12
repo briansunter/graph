1d89156a5531851ac7ef3efb01ede97a091a41aa
Brian Sunter <2816289+briansunter@users.noreply.github.com>
2023-12-11 22:03:29 -1000

    fix redirect

--- site/pages/tags/+onBeforePrerenderStart.ts
+++ site/pages/tags/+onBeforePrerenderStart.ts
@@ -1,10 +1,4 @@
-import { getFiles } from '../../lib/fileProcessor'
-import type { OnBeforePrerenderStartAsync, PageContext } from 'vike/types';
-import { DateTime } from 'luxon';
-import path from 'path';
-import { ImageProcessor } from '../../lib/ImageProcessor'; // Update this import based on your project structure
-
-const imageProcessor = ImageProcessor.getInstance();
+import { getFiles } from "../../lib/fileProcessor";
 
 const onBeforePrerenderStart: OnBeforePrerenderStartAsync = async (): ReturnType<OnBeforePrerenderStartAsync> => {
   const urlToPageMap = await getFiles();
@@ -17,25 +11,21 @@ const onBeforePrerenderStart: OnBeforePrerenderStartAsync = async (): ReturnType
     }
   });
 
-    const pages = Array.from(tags).map(async (tag) => {
-      let redirectTo;
-      if (tag === 'newsletter') {
-        redirectTo = '/newsletter';
-      }
+  const pages = Array.from(tags).map(async (tag) => {
+    let redirectTo;
+    if (tag === 'newsletter') {
+      redirectTo = '/newsletter';
+    }
 
-      const pagesWithTag = Object.values(urlToPageMap).filter((page) => {
-        return page.props.tags && page.props.tags.includes(tag);
-      });
+    const pagesWithTag = Object.values(urlToPageMap).filter((page) => {
+      return page.props.tags && page.props.tags.includes(tag);
+    });
 
     const capitalizedPageTitle = tag.charAt(0).toUpperCase() + tag.slice(1);
 
-    let urlForTag = `/tags/${tag}`;
 
-    if (tag === 'newsletter') {
-      urlForTag = '/newsletter'
-    }
     return {
-      url: urlForTag,
+      url: `/tags/${tag}`,
       pageContext: {
         title: `${capitalizedPageTitle}`,
         description: `Posts tagged with ${tag}`,
@@ -47,6 +37,23 @@ const onBeforePrerenderStart: OnBeforePrerenderStartAsync = async (): ReturnType
       },
     };
   });
+
+  // Add /newsletter page with no redirectTo
+  const newsletterPage = {
+    url: '/newsletter',
+    pageContext: {
+      title: 'Newsletter',
+      description: 'Posts tagged with newsletter',
+      pageProps: {
+        title: 'Newsletter',
+        pages: Object.values(urlToPageMap).filter((page) => {
+          return page.props.tags && page.props.tags.includes('newsletter');
+        }),
+      },
+    },
+  };
+
+  pages.push(Promise.resolve(newsletterPage));
 
   return Promise.all(pages); 
 };
