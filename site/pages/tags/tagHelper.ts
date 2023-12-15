31a1eeb1139bbe1d46f3a5f8e556e1ffc0b24319
Brian Sunter <2816289+briansunter@users.noreply.github.com>
2023-12-14 15:36:13 -1000

    Update organization

--- site/pages/tags/tagHelper.ts
+++ site/pages/tags/tagHelper.ts
@@ -1,11 +1,12 @@
 import { getFiles } from "../../lib/fileProcessor";
 import { Page } from "../pages/+onBeforeRender";
+import { DateTime } from "luxon";
 
 export async function getPagesWithTag(tag: string) {
     const files = await getFiles();
-    const pagesWithTag = Object.values(files).filter((file: Page) => {
-        return file.props.tags && file.props.tags.includes(tag);
-    });
+    const pagesWithTag = Object.values(files)
+    .filter((file: Page) => file.props.tags && file.props.tags.includes(tag))
+    .sort((a: Page, b: Page) => DateTime.fromISO(b.props.date).toMillis() - DateTime.fromISO(a.props.date).toMillis());
 
     return pagesWithTag;
 }
