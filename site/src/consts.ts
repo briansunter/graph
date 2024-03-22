8e5babf0b91e2d50d4ad44cb74179f480b6d1af8
Brian Sunter <2816289+briansunter@users.noreply.github.com>
2024-03-21 16:04:33 -1000

    remove css

--- site/src/consts.ts
+++ site/src/consts.ts
@@ -5,3 +5,16 @@ export const SITE_TITLE = 'Brian Sunter';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 
 export const NEWSLETTER_LINK = 'https://newsletter.briansunter.com';
+
+export const SOCIAL_ICONS = [
+    { name: "X", url: "https://twitter.com/bsunter", icon: "square-x-twitter.svg" },
+    { name: "email", url: "https://twitter.com/bsunter", icon: "email.svg" },
+    { name: "GitHub", url: "https://github.com/briansunter", icon: "square-github.svg" },
+    { name: "LinkedIn", url: "https://www.linkedin.com/in/briansunter/", icon: "linkedin.svg" },
+    { name: "YouTube", url: "https://www.youtube.com/channel/UC4Nu4dncIcTC1DyOG5XG2Vg", icon: "youtube.svg" },
+    { name: "RSS", url: "https://briansunter.com/index.xml", icon: "square-rss.svg" },
+    { name: "Buy Coffee", url: "https://buymeacoffee.com/bsunter", icon: "coffee.svg" },
+    { name: "bitcoin", url: "/bitcoin", icon: "bitcoin.svg" },
+    { name: "instagram", url: "https://www.instagram.com/bsunter/", icon: "square-instagram.svg" },
+    { name: "matrix", url: "https://matrix.to/#/@bsunter:matrix.org", icon: "matrix.svg" },
+  ];
